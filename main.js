4754e61cdd74b91ccdc3afa002ee461d7245a7ae
HieuNguyen4603 <111641710+HieeusNguyen@users.noreply.github.com>
2023-06-08 19:41:46 +0700

    1 file modified, 1 deleted

--- main.js
+++
@@ -1,7 +0,0 @@
-var close = document.querySelector(".close-btn");
-close.addEventListener("click", handle);
-function handle()
-{
-  var modal = document.querySelector(".modal");
-  modal.style.display = "none";
-}
